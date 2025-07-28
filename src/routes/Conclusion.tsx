a3d079c580389f8dbeb360266e8a94bd508febd7
Daniel Chapdelanie <daniel.chapdelaine@vitalsource.com>
2025-07-28 10:07:23 -0500

    modify conclusion

--- src/routes/Conclusion.tsx
+++ src/routes/Conclusion.tsx
@@ -6,19 +6,18 @@ function Conclusion() {
       <div style={{ fontFamily: 'monospace', fontSize: '14px', lineHeight: '1.5' }}>
         <div style={{marginBottom: '3rem'}}> How did it go? Did it work? If it did:</div>
         <div>
-          <strong>Benefits could be:</strong>
+          <strong>Benefits:</strong>
           <ul>
             <li>Simpler development: Fewer PRs, less cognitive overhead</li>
             <li>Safer deployments: Less risk of downtime or client errors</li>
             <li>Faster feature delivery: No strict order constraints</li>
-            <li>Improved developer happiness: Reduced complexity and mental load</li>
           </ul>
         </div>
         <div>
           <strong>Risks:</strong>
           <ul>
-            <li>What if the ai guesses incorrectly?</li>
-            <li>Performance</li>
+            <li>Incorrect Guess: Difficult to determine accuracy since there are no errors</li>
+            <li>Performance Impact: Calling out to an api for small adjustments is expensive</li>
           </ul>
         </div>
         <div style={{marginTop: '3rem'}}> Questions?</div>
